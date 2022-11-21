97bca36ebf17f1992a7f71777ad8a9a28fa96344
Alex Chan <alexchan5787@gmail.com>
2022-11-20 19:21:46 -0500

    Fixed styling.

--- movie/src/components/HelloWorld.vue
+++ movie/src/components/HelloWorld.vue
@@ -1,37 +1,16 @@
-      <template>
-        <!-- <div id="getform">
-          <h1>Welcome to a Movie Searcher!</h1>
-        <div>
-          <select class="select" v-model = "select" @change = "">
-            <option value="829280">Enola Holmes 2</option>
-            <option value="505642">Black Panther: Wakanda Forever</option>
-            <option value="269149">Zootopia</option>
-            <option value="616037">Thor: Love and Thunder</option>
-            <option value="1037858">The Soccer Football Movie</option>
-            <option value="985939">Fall</option>
-            <option value="14160">Up</option>
-            <option value="436270">Black Adam</option>
-            <option value="766475">See How They Run</option>
-            <option value="771">Home Alone</option>
-            <option value="772">Home Alone 2</option>
-          </select>
-          <form class="get" @submit.prevent = "getMovie()">
-            <input type="submit" value="GET" class="getButton"/>
-          </form>
-          </div>
-        </div> -->
+<template>
 <h1>Welcome to a Movie Searcher!</h1>
 <div id = "getform">
   <form class = "form" @submit.prevent = "findMovie()">
     <input type="search" class = "search" placeholder = "Search Movies Here!" ref = "search">
     <input type ="submit" class = "submit"/>
   </form>
-    <form class = "get" @submit.prevent = "getMovie()">
-        <input type = "submit" value = "GET" class = "getButton"/>
-    </form>
-    <select class="select" ref = "select" @change = "">
-      <option  v-for = "ids in values" :value = "ids.id">{{ids.title}}</option>
-    </select>
+  <form class = "get" @submit.prevent = "getMovie()">
+      <input type = "submit" value = "GET" class = "getButton"/>
+  </form>
+  <select class="select" ref = "select" @change = "">
+    <option  v-for = "ids in values" :value = "ids.id">{{ids.title}}</option>
+  </select>
 </div>
 
 
@@ -53,6 +32,7 @@
 <script setup>
 import { ref, defineComponent, VueElement} from 'vue'
 import axios from 'axios';
+
 const select = ref()
 const search = ref();
 const values = ref([])
@@ -72,6 +52,7 @@ let findMovie = function(){
       query: query,
     },
   });
+
   response = response.then((moviesData) => {
     for (let movie of moviesData.data.results) {
       this.values.push({
@@ -83,17 +64,17 @@ let findMovie = function(){
   });
 }
 
-
 const getMovie = function() {
-      let specific = axios.get(
-    `https://api.themoviedb.org/3/movie/${this.select.value}`,
-    {
-      params: {
-        api_key: "dae84c7eae2f2c63d4bdf1e712d56304",
-        append_to_response: "videos",
-      },
-    }
-  )
+
+let specific = axios.get(
+  `https://api.themoviedb.org/3/movie/${this.select.value}`,{
+    params: {
+      api_key: "dae84c7eae2f2c63d4bdf1e712d56304",
+      append_to_response: "videos",
+    },
+  }
+)
+
   specific = specific.then((specificData) => {
     this.loaded = true
     var g = "";
@@ -111,17 +92,14 @@ const getMovie = function() {
         prod += specificData.data.production_countries[i].name + ", ";
       }
   
-      if (
-        specificData.data.videos.results.filter(
-          (trailer) => trailer.type === "Trailer"
-        ).length > 0
-        ) {
+      if (specificData.data.videos.results.filter((trailer) => trailer.type === "Trailer").length > 0) {
           this.traileriframe = `https://www.youtube.com/embed/${
             specificData.data.videos.results
             .filter((trailer) => trailer.type === "Trailer")
             .at(0).key
         }`;
       }
+
       this.title = specificData.data.title
       this.img = `https://image.tmdb.org/t/p/w500${specificData.data.poster_path}`;
       this.p = `Status: ${specificData.data.status}
@@ -130,7 +108,8 @@ const getMovie = function() {
       <br> Genre: ${g} <br> Production Companies: ${pc}  -- Production Countries: ${prod}
       <br> Runtime: ${specificData.data.runtime} minutes
       <br> Homepage: <a href = ${specificData.data.homepage}>${specificData.data.homepage}</a> `;
-        this.video = [];
+      
+      this.video = [];
       for (let i = 0; i < specificData.data.videos.results.length; i++) {
         if (specificData.data.videos.results.at(i).type != "Trailer") {
           this.video.push(`https://www.youtube.com/embed/${specificData.data.videos.results.at(i).key}`);
@@ -138,10 +117,18 @@ const getMovie = function() {
         } 
       });
     }
+
 </script>
  
  
 <style scoped>
+
+div{
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
 .title{
   text-align: center;
   width: 100vw;
@@ -177,7 +164,7 @@ iframe{
 #getform{
   width: 100vw;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   padding-bottom: 10px;
@@ -197,6 +184,8 @@ box-shadow: 0px 1px 5px rgb(255 255 255);;
 
 h1{
   font-size: 50px;
+  width: 100vw;
+  text-align: center;
 }
 
 .extraDiv{
@@ -240,11 +229,11 @@ iframe{
 img{
   height: 281.25px;
 }
-/* .movieInfo{ */
-  /* padding: 1rem 2rem;
+.movieInfo{
+  padding: 1rem 2rem;
   background-color: rgba(43, 43, 43, 0.5);
   border-top: 5px solid rgb(255,255,255,0.1);
-  border-bottom: 5px solid rgb(255,255,255,0.1); */
-/* } */
+  border-bottom: 5px solid rgb(255,255,255,0.1);
+}
 </style>
 
